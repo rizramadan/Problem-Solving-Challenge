eedc441e8dcb20fe0366f196e254aebbec1d3ca0
Rizky Ramadan <75460067+rizramadan@users.noreply.github.com>
2021-10-12 13:03:17 +0700

    Problem-5

---
+++ Assets/Scripts/Movement/ArrowMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D Rigidbody2D;
+
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(xInput, yInput).normalized;
+
+        Rigidbody2D.velocity = direction * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/Movement/MouseMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float moveConstraint = 5.5f;
+    private Vector3 targetMove;
+
+    private Rigidbody2D Rigidbody2D;
+
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mousePos.x < moveConstraint && mousePos.x > -moveConstraint
+                && mousePos.y < moveConstraint && mousePos.y > -moveConstraint)
+            {
+                targetMove = mousePos;
+                targetMove.z = 0;
+            }
+        }
+
+        if (transform.position != targetMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetMove, moveSpeed * Time.deltaTime);
+        }
+    }
+}
