b907c842e6ee88d156839e7f4c33a3c6971e9fb3
Rizky Ramadan <75460067+rizramadan@users.noreply.github.com>
2021-10-12 18:17:31 +0700

    Problem-10

---
+++ Assets/Scripts/Movement/BallControll_Prob7.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControll_Prob7 : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D Rigidbody2D;
+    public ScoreController scoreController;
+
+    private void Start()
+    {
+
+        ResetBall();
+
+
+    }
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(xInput, yInput).normalized;
+
+        Rigidbody2D.velocity = direction * moveSpeed;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Box")
+        {
+            scoreController.IncreaseCurrentScore(1);
+            Destroy(collision.gameObject);
+           
+        }
+    }
+   
+
+
+    private void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        Rigidbody2D.velocity = Vector2.zero;
+    }
+
+    private void PushBall()
+    {
+        Rigidbody2D.AddForce(new Vector2(10f, 8f));
+    }
+}
---
+++ Assets/Scripts/Score/ScoreController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text score;
+    public Text gameoverScore;
+
+    private int currentScore;
+
+    private void Start()
+    {
+        currentScore = 0;
+    }
+
+    private void Update()
+    {
+        score.text = $"Score : { currentScore.ToString("0") }";
+    }
+
+    public void IncreaseCurrentScore(int num)
+    {
+        currentScore += num;
+    }
+
+    public void EndScore()
+    {
+        gameoverScore.text = $"Score : { currentScore.ToString("0") }";
+    }
+}
